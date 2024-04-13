459a6964b418bba79018ebf0930f20d3bc86b6ae
jwpark1211 <1211abc@naver.com>
2024-04-13 22:14:46 +0900

    init project

---
+++ src/main/java/redisEx/demo413/Demo413Application.java
@@ -0,0 +1,13 @@
+package redisEx.demo413;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo413Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demo413Application.class, args);
+	}
+
+}
---
+++ src/test/java/redisEx/demo413/Demo413ApplicationTests.java
@@ -0,0 +1,13 @@
+package redisEx.demo413;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo413ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
